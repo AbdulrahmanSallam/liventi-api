2670dc7c3f85eee08175d9ae5c6ed9ccf9cabacf
Abdulrahman Sallam <abdulrahman.sallam.official@gmail.com>
2026-08-11 20:48:00 +0300

    Implement search apartments

---
+++ src/Liventi.Application/Apartments/SearchApartments/SearchApartmentsQuery.cs
@@ -0,0 +1,7 @@
+﻿using Liventi.Application.Abstractions.Messaging;
+
+namespace Liventi.Application.Apartments.SearchApartments;
+
+public sealed record SearchApartmentsQuery(
+    DateOnly StartDate,
+    DateOnly EndDate) : IQuery<IReadOnlyList<ApartmentResponse>>;
--- src/Liventi.Application/Bookings/GetBooking/BookingResponse.cs
+++ src/Liventi.Application/Bookings/GetBooking/BookingResponse.cs
@@ -12,19 +12,19 @@ public sealed class BookingResponse
 
     public decimal PriceAmount { get; init; }
 
-    public string PriceCurrency { get; init; } = null!;
+    public string PriceCurrency { get; init; }
 
     public decimal CleaningFeeAmount { get; init; }
 
-    public string CleaningFeeCurrency { get; init; } = null!;
+    public string CleaningFeeCurrency { get; init; }
 
     public decimal AmenitiesUpChargeAmount { get; init; }
 
-    public string AmenitiesUpChargeCurrency { get; init; } = null!;
+    public string AmenitiesUpChargeCurrency { get; init; }
 
     public decimal TotalPriceAmount { get; init; }
 
-    public string TotalPriceCurrency { get; init; } = null!;
+    public string TotalPriceCurrency { get; init; }
 
     public DateOnly DurationStart { get; init; }
 
