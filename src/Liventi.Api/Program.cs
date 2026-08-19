ada100730855f5729fa167ff3a84e4d125a5ca35
Abdulrahman Sallam <abdulrahman.sallam.official@gmail.com>
2026-08-19 10:36:52 +0300

    Implement Health checks

--- src/Liventi.Api/Program.cs
+++ src/Liventi.Api/Program.cs
@@ -1,31 +1,33 @@
 using Liventi.Api.Extensions;
 using Liventi.Application;
 using Liventi.Infrastructure;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 {
-
     builder.Host.UseSerilog((context, loggerConfig) =>
-        loggerConfig.ReadFrom.Configuration(context.Configuration));
+    loggerConfig.ReadFrom.Configuration(context.Configuration));
 
     builder.Services.AddControllers();
 
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 
-
     builder.Services.AddApplication();
     builder.Services.AddInfrastructure(builder.Configuration);
+
 }
+
 var app = builder.Build();
 
 {
     if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
-        app.UseSwaggerUI();
+
 
         app.ApplyMigrations();
 
@@ -47,9 +49,10 @@ var app = builder.Build();
 
     app.MapControllers();
 
+    app.MapHealthChecks("health", new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 }
 
-
-
 app.Run();
-
